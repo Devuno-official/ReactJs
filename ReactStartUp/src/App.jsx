08056cf215ef781e3baa80ca87e183a3e42ac958
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-09-15 17:53:34 +0500

    My First React App

---
+++ ReactStartUp/src/App.jsx
@@ -0,0 +1,12 @@
+
+let App = function(){
+     return (
+        <div>
+            <h1>Start React js </h1>
+            <p>This is my first React app</p>
+            <button>Click Me</button>
+        </div>
+     );
+}
+
+export default App;
