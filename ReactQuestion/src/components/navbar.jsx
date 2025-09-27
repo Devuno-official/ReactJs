7c938bd6b02768298bc2de8a91a9d2f57d21b5ea
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-09-27 16:34:51 +0500

    My mini react practice question with tailwindcss

---
+++ ReactQuestion/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+
+export default function Navbar() {
+     return (
+          <>
+               <div className="navbar w-full h-[6%]  bg-amber-200 fixed top-0 flex items-center justify-items-center place-content-around ">
+                    <a href="#">Contact</a>
+                    <a href="#">Setting</a>
+                    <a href="#">Profile</a>
+                    <a href="#">Portfolio</a>
+                    <a href="#">help</a>
+                    <input type="text" name="Search" placeholder="Search Here ..." className="bar placeholder: text-black border-1 rounded-2xl w-[30%] h-[30px]  font-mono" />
+               </div>
+          </>
+     );
+}
