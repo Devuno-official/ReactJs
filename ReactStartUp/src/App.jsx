f4835decd54f80a52f3b1d9d068fcf8566f79e6f
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-10-06 14:13:18 +0500

    parent files in react

--- ReactStartUp/src/App.jsx
+++ ReactStartUp/src/App.jsx
@@ -1,16 +1,26 @@
 import Rendering from "./components/renderingLists";
 import CardProduct from "./components/CardProduct";
+import HandleEvent from "./components/HandleEvent";
+
 const user = [
   { title: "Banana", color: "pink", id: 1 },
   { title: "tomotes", color: "lightblue", id: 2 },
   { title: "watermelon", color: "blue", id: 3 },
 ];
 
+const ClickEvent = function(e) {
+  e.stopPropagation();         //* react special syntheticBaseEvent object 
+  document.body.style = "Black";
+}
+
+
+
 function App() {
   return (
     <>
-     <Rendering />
-      <CardProduct  items={user} />
+      <Rendering />
+      <HandleEvent ClickEvent={ClickEvent} /> {/*pass function via props */}
+      <CardProduct items={user} />
     </>
   );
 }
