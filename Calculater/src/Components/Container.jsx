4d5b306e85da9205e7ea3c9a804142bc0a607542
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-10-10 17:52:30 +0500

    Container that contain  prop children

--- Calculater/src/Components/Container.jsx
+++ Calculater/src/Components/Container.jsx
@@ -1,6 +1,11 @@
-
 let Container = (props) => {
-     return <div className=" h-screen bg-green-500 flex items-center justify-center flex-col ">{props.children}</div>;
-}
+  return (
+    <div className="min-h-screen w-full bg-gradient-to-b from-gray-800 to-gray-900 flex items-center justify-center p-4 sm:p-6 md:p-8">
+      <div className="w-full max-w-[320px] sm:max-w-[360px] flex flex-col items-center">
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
 export default Container;
