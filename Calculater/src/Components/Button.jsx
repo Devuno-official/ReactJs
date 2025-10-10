a0ce7757303214a95c7e1df8af2cb6782d6477c2
Devuno-official <185803128+Yasircodingbt@users.noreply.github.com>
2025-10-10 17:50:43 +0500

    button component

--- Calculater/src/Components/Button.jsx
+++ Calculater/src/Components/Button.jsx
@@ -1,10 +1,29 @@
+let Btn = function ({ symbol, showValHandle }) {
+  return symbol.map((arr) => {
+    const getButtonStyle = () => {
+      if (arr === "C") return "bg-zinc-600 hover:bg-zinc-700";
+      if (arr === "=") return "bg-orange-500 hover:bg-orange-600";
+      if (["+", "-", "*", "/", "(", ")"].includes(arr))
+        return "bg-orange-500 hover:bg-orange-600";
+      return "bg-zinc-800 hover:bg-zinc-700";
+    };
 
-let Btn = function ({symbol}) {
- return symbol.map((arr) => {
-          return (
-               <button type="button" className=" bg-linear-to-t from-sky-500 to-indigo-500 h-18 w-full  text-amber-100 rounded-2xl">{arr}</button>
-          );
-     })
-}
+    return (
+      <button
+        key={arr}
+        type="button"
+        className={`${getButtonStyle()}
+                         aspect-square w-full
+                         flex items-center justify-center
+                         rounded-full text-white text-2xl sm:text-3xl font-medium
+                         transition-colors duration-150
+                         active:brightness-75`}
+        onClick={showValHandle}
+      >
+        {arr}
+      </button>
+    );
+  });
+};
 
 export default Btn;
