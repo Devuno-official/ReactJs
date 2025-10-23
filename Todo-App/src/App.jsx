ae99dd38a97d3f72a3273167b008acf6699bc124
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-10-23 18:12:20 +0500

    now adding tasks increase the taskCount on the header

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -2,22 +2,26 @@ import ShowTask from "./Components/showTask";
 import Modal from "./Components/Modal";
 import { useState } from "react";
 import Container from "./Components/Container";
+import Header from "./Components/Header";
 
 function App() {
-  let [userTask, setuserVal] = useState([]);
+  let [userTask, setuserTask] = useState([]);
   let [heading, setheading] = useState("");
   let [task, settask] = useState("");
+  let [taskCount , settaskCount] = useState(0);
 
   function HeadingHandle(e) {
-    e.stopPropagation();
-    heading = `${e.target.value}`;
-    setheading(heading);
+     //.stopPropagation();
+    heading = e.target.value;
+    console.log(heading);
+    setheading(heading.trim());
   }
 
   function TaskHandle(e) {
-    e.stopPropagation();
-    task = `${e.target.value}`;
-    settask(task);
+    //stopPropagation();
+      task= e.target.value
+      console.log(task);
+      settask(task.trim());
   }
 
   function saveHandle() {
@@ -26,13 +30,15 @@ function App() {
       task: task,
       id: 2,
     };
-    let Val = [...userTask,storeTask];
-    setuserVal(Val);
+    let Val = [...userTask, storeTask];
+    setuserTask(Val);
+    settaskCount(userTask.length + 1);
   }
-
+      
   return (
     <>
       <Container>
+        <Header taskCount={taskCount} />
         <ShowTask userTask={userTask} />
         <Modal
           HeadingHandle={HeadingHandle}
