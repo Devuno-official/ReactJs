594f7862236596a74884f2354f66283eecf5e5bb
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-10-24 17:24:02 +0500

    on everytask key well update automatic

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -8,42 +8,50 @@ function App() {
   let [userTask, setuserTask] = useState([]);
   let [heading, setheading] = useState("");
   let [task, settask] = useState("");
-  let [taskCount , settaskCount] = useState(0);
+  let [taskCount, settaskCount] = useState(0);
 
   function HeadingHandle(e) {
-     //.stopPropagation();
-    heading = e.target.value;
-    console.log(heading);
-    setheading(heading.trim());
+   // e.stopPropagation();
+     heading =+ e.target.value;
+     heading = heading.trim();
+       setheading(heading);
   }
 
   function TaskHandle(e) {
-    //stopPropagation();
-      task= e.target.value
-      console.log(task);
-      settask(task.trim());
+     // e.stopPropagation();
+    task =+ e.target.value; 
+    settask(task.trim());
   }
 
   function saveHandle() {
+    
+     console.log(task);
+     console.log(heading);
+    
     let storeTask = {
       heading: heading,
       task: task,
-      id: 2,
+      id: taskCount,
     };
-    let Val = [...userTask, storeTask];
-    setuserTask(Val);
-    settaskCount(userTask.length + 1);
+
+    if (storeTask.heading.length > 0 && storeTask.task.length > 0) {
+      let Val = [...userTask, storeTask];
+      setuserTask(Val);
+      settaskCount(userTask.length + 1);
+    } 
   }
-      
+
   return (
     <>
       <Container>
         <Header taskCount={taskCount} />
         <ShowTask userTask={userTask} />
         <Modal
-          HeadingHandle={HeadingHandle}
+         HeadingHandle={HeadingHandle}
           TaskHandle={TaskHandle}
           saveHandle={saveHandle}
+          heading={heading}
+          task={task}
         />
       </Container>
     </>
